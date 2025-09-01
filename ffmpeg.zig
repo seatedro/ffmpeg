7a2afc2eb9800a4aa5e0e2e43d5359da1e29d819
ro <git@seated.ro>
2025-09-01 14:02:33 -0700

    libavfilter

--- ffmpeg.zig
+++ ffmpeg.zig
@@ -8,4 +8,8 @@ pub const c = @cImport({
     @cInclude("libswresample/swresample.h");
     @cInclude("libavutil/channel_layout.h");
     @cInclude("libavutil/samplefmt.h");
+    // Filters for GIF palettegen/paletteuse pipeline
+    @cInclude("libavfilter/avfilter.h");
+    @cInclude("libavfilter/buffersrc.h");
+    @cInclude("libavfilter/buffersink.h");
 });
